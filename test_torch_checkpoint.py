32809b2ab6438aee5883b13c5a10edfa2eb0a3e1
Huihuo Zheng <zhenghh04@gmail.com>
2025-02-26 10:53:26 -0600

    added fix

--- test_torch_checkpoint.py
+++ test_torch_checkpoint.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--output-folder', type=str, default="/tmp/")  
 
 args = parser.parse_args()
-args.
+
 a=torch.Tensor((100))
 b=torch.Tensor((1048576))
 c=torch.Tensor((1637385))
